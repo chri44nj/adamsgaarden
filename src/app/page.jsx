d52499129a97a30f810d0fe2d8b5eade6fb0782f
Valentin <Chris_valentin@hotmail.com>
2024-04-07 16:10:12 +0200

    Footer påbegyndt

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,6 +9,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.frontPageMain}>
+      <h1>Adamsgården</h1>
       <p>Der står sgu noget fed tekst her!</p>
     </main>
   );
