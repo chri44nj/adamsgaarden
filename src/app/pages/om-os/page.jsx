7dc296671b6f1082cad22c7ac0e6b5e3e8d7a538
Valentin <Chris_valentin@hotmail.com>
2024-04-11 18:58:21 +0200

    blue icons

--- src/app/pages/om-os/page.jsx
+++ src/app/pages/om-os/page.jsx
@@ -66,13 +66,13 @@ export default function Aboutus() {
         </div>
         <div className={styles.flexRow}>
           <div className="flex-row">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ff5555" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#3388cc" class="bi bi-telephone-fill" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
             </svg>
             <a href="tel:+4522264550">22 26 45 50</a>
           </div>
           <a className={`${styles.link} flex-row`} target="_blank" href="mailto:mail@adamsgaarden.dk">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffbb33" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#3388cc" class="bi bi-envelope-fill" viewBox="0 0 16 16">
               <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z" />
             </svg>
             mail@adamsgaarden.dk
