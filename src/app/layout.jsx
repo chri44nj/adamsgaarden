9d79135c842aa80a4656ada378911f8f8ebadd5c
Christian Valentin <chris_valentin@hotmail.com>
2024-05-02 14:09:43 +0200

    meta image

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,6 +13,7 @@ import Contexts from "./components/Contexts";
 export const metadata = {
   title: "Adamsgården",
   description: "Besøg vores hyggelige gårdbutik på Adamsgården, i den idylliske udkant af Jyderup.",
+  image: "/pics/linkImage.png",
 };
 
 export default function RootLayout({ children }) {
@@ -21,6 +22,13 @@ export default function RootLayout({ children }) {
       <Contexts>
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <meta property="og:title" content={metadata.title} />
+          <meta property="og:description" content={metadata.description} />
+          <meta property="og:image" content={metadata.image} />
+          <meta property="og:url" content={typeof window !== "undefined" && window.location.href} />
+          <meta property="og:type" content="website" />
+          <meta property="og:locale" content="da_DK" />
+          <meta property="og:site_name" content="Adamsgården" />
         </Head>
         <html lang="da">
           <body>
