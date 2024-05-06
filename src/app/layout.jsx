258525762bff8828865699f1ab31ff592499f7b0
Christian Valentin <chris_valentin@hotmail.com>
2024-05-06 19:48:11 +0200

    meta

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -22,8 +22,8 @@ export default function RootLayout({ children }) {
       <Contexts>
         <Head>
           <link rel="icon" href="/favicon.jpg" />
-          <meta property="og:title" content={metadata.title} />
-          <meta property="og:description" content={metadata.description} />
+          <meta property="og:title" content="Adamsgården" />
+          <meta property="og:description" content="Besøg vores hyggelige gårdbutik på Adamsgården, i den idylliske udkant af Jyderup." />
           <meta property="og:image" content="/pics/linkImage.png" />
           <meta property="og:url" content={typeof window !== "undefined" && window.location.href} />
           <meta property="og:type" content="website" />
